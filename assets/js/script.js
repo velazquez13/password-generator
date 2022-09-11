3df128d5d2926d1a47f531fffa8099f5ce31d55a
velazquez13 <alinamvelazquez@gmail.com>
2022-09-11 00:15:46 -0700

    updated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  var lowercharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbercharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var othercharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+  var selectedArray = [];
+
   passwordText.value = password;
 }
 
@@ -17,4 +23,34 @@ function generatePassword() {
   var lettersUppercase = confirm("Include upper-case letters?");
   var lettersLowercase = confirm("Include lower-case letters?");
   var other = confirm("Inlcude symbols and special characters?");
+}
+if (numbers){
+  resultArray = resultArray.concat(numbers);
+  
+}
+if (lettersUppercase){
+  resultArray = resultArray.concat(lettersUppercase);
+
+}
+if (lettersLowercase){
+  resultArray = resultArray.concat(lettersLowercase);
+
+}
+if (other){
+  resultArray = resultArray.concat(other);
+}
+console.log(resultArray)
+
+for (var i = 0; i < numCharacter; i++) {
+      
+  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
+  }
+
+  return userArray.join("") ;
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
